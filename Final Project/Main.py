04cafb4cdf0d89f8655f3f5177d1e0a69553daa9
n00dl3r <addisonnewnan@gmail.com>
2022-12-15 20:34:42 -0800

    bug fix, reorganized

--- Final Project/Main.py
+++ Final Project/Main.py
@@ -32,37 +32,6 @@ count = 0
 pg.init()
 pg.mixer.init()
 
-#Game class 
-class Game:
-    def __init__(self):
-        #Takes settings and imports them to the code with then 
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption(TITLE)
-        #Sets core of game by initalizing the clock
-        self.clock = pg.time.Clock()
-        self.running = running = True
-        self.font_name = pg.font.match_font(FONT_NAME)
-        self.load_data()
-        self.reaction_time
-        
-        #Creates all the text used in the file
-    def draw_text(self, text, size, color, x, y):
-            font = pg.font.Font(self.font_name, size)
-            text_surface = font.render(text, True, color)
-            text_rect = text_surface.get_rect()
-            text_rect.midtop = (x, y)
-            self.screen.blit(text_surface, text_rect)
-
-    #Fetches high score data 
-    def load_data(self):
-        # load high score
-        self.dir = path.dirname(__file__)
-        with open(path.join(self.dir, HS_FILE), 'r') as f:
-            try:
-                self.highscore = int(f.read())
-            except:
-                self.highscore = 0
-
         # Game Loop
         #real time is the game time so that we can diffrenciate the play time from avg reaction time
         #started loop and created a ticker to create ingame time
@@ -100,6 +69,38 @@ if game_state == "wait_for_reaction":
                         if realtime >= game_time:
                             game_state = "wait_for_reaction"        
 
+#Game class 
+class Game:
+    def __init__(self):
+        #Takes settings and imports them to the code with then 
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        #Sets core of game by initalizing the clock
+        self.clock = pg.time.Clock()
+        self.running = running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
+        self.load_data()
+        #self.reaction_time
+        
+        #Creates all the text used in the file
+    def draw_text(self, text, size, color, x, y):
+            font = pg.font.Font(self.font_name, size)
+            text_surface = font.render(text, True, color)
+            text_rect = text_surface.get_rect()
+            text_rect.midtop = (x, y)
+            self.screen.blit(text_surface, text_rect)
+
+    #Fetches high score data 
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        with open(path.join(self.dir, HS_FILE), 'r') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+
+
 # refreshes the page basicaly and make teh entire window black again
 SCREEN.fill(pg.Color("BLACK"))
 
@@ -116,40 +117,38 @@ if reaction:
 if avg_reaction:
         SCREEN.blit(avg_reaction, avg_reaction.get_rect(center = (center[0], 350)))
 
-         
+def show_start_screen(self):
+        # game splash/start screen
+        realtime = pg.time.get_ticks()
+        game_time = realtime + random.randint(1000, 4000)
+        reaction_time = (realtime - game_time) / 1000
+        reaction = FONT_NAME(f"REACTION TIME: {reaction_time:.03f}",0,(255,255,255))
+        avg_reaction = FONT_NAME(f"AVERAGE REACTION TIME IS: {avg_reaction:.03f}",0,(255,255,255))
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        self.draw_text("Your Average Reaction: " + str(avg_reaction), 22, WHITE, WIDTH / 2, 15)
+        pg.display.flip()
 
-# def show_start_screen(self):
-#         # game splash/start screen
-#         realtime = pg.time.get_ticks()
-#         game_time = realtime + random.randint(1000, 4000)
-#         reaction_time = (realtime - game_time) / 1000
-#         reaction = FONT_NAME(f"REACTION TIME: {reaction_time:.03f}",0,(255,255,255))
-#         avg_reaction = FONT_NAME(f"AVERAGE REACTION TIME IS: {avg_reaction:.03f}",0,(255,255,255))
-#         self.screen.fill(BGCOLOR)
-#         self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
-#         self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
-#         self.draw_text("Your Average Reaction: " + str(avg_reaction), 22, WHITE, WIDTH / 2, 15)
-#         pg.display.flip()
+    def score(self):
 
-    # def score(self):
-
-# def show_go_screen(self):
-#         # game over/continue
-#         if not self.running:
-#             return
-#         self.screen.fill(BLACK)
-#         #screen display
-#         self.draw_TEXT("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
-#         self.draw_TEXT("time: " + str(self.score), 22, WHITE, WIDTH / 2, HEIGHT / 2)
-#         self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
-#         if self.score > self.highscore:
-#             self.highscore = self.score
-#             self.draw_text("FASTEST TIME!", 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
-#             with open(path.join(self.dir, HS_FILE), 'w') as f:
-#                 f.write(str(self.score))
-#         #actualy calculates the fastest time
-#         else:
-#             self.draw_text("Fastest time: " + str(self.highscore), 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
-#         pg.display.flip()
-# Game()
+def show_go_screen(self):
+        # game over/continue
+        if not self.running:
+            return
+        self.screen.fill(BLACK)
+        #screen display
+        self.draw_TEXT("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_TEXT("time: " + str(self.score), 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.draw_text("FASTEST TIME!", 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
+            with open(path.join(self.dir, HS_FILE), 'w') as f:
+                f.write(str(self.score))
+        #actualy calculates the fastest time
+        else:
+            self.draw_text("Fastest time: " + str(self.highscore), 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
+        pg.display.flip()
+Game()
 pg.quit()
